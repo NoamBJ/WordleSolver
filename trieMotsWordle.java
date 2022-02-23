727aee73bef5db7492199c7f3bca970f379ac6df
Noam <noambires@gmail.com>
2022-02-23 11:03:28 +0100

    tres opti

--- trieMotsWordle.java
+++ trieMotsWordle.java
@@ -13,21 +13,7 @@ public class trieMotsWordle {
         // Le texte est ensuite decoupe en groupes de lettres
         String[] tableauGroupesLettres = leTexte.decoupe();
 
-        HashSet<String> mapMots = new HashSet<String>();
-
-        for (int i = 0; i < tableauGroupesLettres.length; i++) {
-
-            String groupeLettres = tableauGroupesLettres[i];
-            mapMots.add(groupeLettres);
-        }
-
-        LinkedList<String> list = new LinkedList<String>();
-
-        // ajoute les mots dans une liste
-
-        for (String key : mapMots) {
-            list.add(key);
-        }
+        List<String> list = Arrays.asList(tableauGroupesLettres);
 
         // trie la liste et ajoute le mot dans le fichier
 
