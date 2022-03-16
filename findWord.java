79bbd2033d8288096f4b503181840d5e40200c5a
Noam <noambires@gmail.com>
2022-03-16 16:51:43 +0100

    class trouver mot

--- findWord.java
+++ findWord.java
@@ -1,26 +1,6 @@
-import java.io.*;
-import java.util.*;
-
-public class findWord {
+public class FindWord {
 
     public static void main(String[] args) {
-
-        String nomDuFichierALire = "dicoOrdered.txt";
-
-        // Lecture du fichier et creation du texte en memoire:
-        Texte leTexte = new Texte(nomDuFichierALire);
-
-        // Le texte est ensuite decoupe en groupes de lettres
-        String[] tableauGroupesLettres = leTexte.decoupe();
-
-        List<String> list = Arrays.asList(tableauGroupesLettres);
-        for (String word : list) {
-            if (word.charAt(4) == 'e' && word.charAt(3) == 'e' && word.contains("r") && !word.contains("s")
-                    && !word.contains("a") && !word.contains("l") && !word.contains("t") && !word.contains("c")
-                    && !word.contains("h") && !word.contains("d") && !word.contains("o")) {
-                System.out.println(word);
-            }
-        }
 
     }
 }
